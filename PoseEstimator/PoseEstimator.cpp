481e51032fff6219ea911a92e30f0b0ad67e922d
Pedro Vieira <pedrovieira@arrowplan.com>
2021-04-27 18:19:42 +0200

    Change file structure

---
+++ PoseEstimator/PoseEstimator.cpp
@@ -0,0 +1,47 @@
+﻿// PoseEstimator.cpp : Defines the entry point for the application.
+
+#include "PoseEstimator.h"
+
+#include <iostream>
+
+#include <opencv2/nonfree/features2d.hpp>
+
+namespace DLR {
+  /**
+    * C'Tor of PoseEstimator. It shall initialize all ressources
+    * needed for the estimation process.
+    * \param camera_matrix intrinsics of the given camera
+    */
+  PoseEstimator::PoseEstimator(const cv::Mat& camera_matrix) : m_camera_matrix(camera_matrix)
+  {
+    // write additional code here
+  }
+
+  /**
+    * Estimate the pose between both given image pairs.
+    * \param source source image pair
+    * \param target target image pair
+    * \return 4x4 pose representing relative pose from
+    *         source to target
+    */
+  Eigen::Matrix4d PoseEstimator::estimate_pose(const rgbd_pair_t& source,
+    const rgbd_pair_t& target)
+  {
+    // Detect SIFT features on source RGB image
+    cv::SiftFeatureDetector source_detector;
+    std::vector<cv::KeyPoint> source_keypoints;
+    source_detector.detect(source.first, source_keypoints);
+
+    // Detect SIFT features on target RGB image
+    cv::SiftFeatureDetector target_detector;
+    std::vector<cv::KeyPoint> target_keypoints;
+    target_detector.detect(target.first, target_keypoints);
+  }
+
+}
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
---
+++ PoseEstimator/PoseEstimator.h
@@ -0,0 +1,50 @@
+﻿/* ************************************
+ * Pose Estimation
+ *
+ * This class shall be able to estimate
+ * the pose between two given images
+ * without scale error.
+ *
+ * For implementation expect all common
+ * 3rdparty libraries to be available
+ * (e.g. Eigen, OpenCV, Boost) and
+ * already embedded into the environment
+ *
+ * ************************************/
+
+#pragma once
+
+#include <opencv2/core.hpp>
+#include <Eigen/Eigen>
+
+#include <utility>
+
+namespace DLR {
+  /**
+   * typedef for syntactic sugar
+   */
+  using rgbd_pair_t = std::pair<cv::Mat, cv::Mat>;
+
+  class PoseEstimator {
+  public:
+    /**
+     * C'Tor of PoseEstimator. It shall initialize all ressources
+     * needed for the estimation process.
+     * \param camera_matrix intrinsics of the given camera
+     */
+    PoseEstimator(const cv::Mat& camera_matrix);
+
+    /**
+     * Estimate the pose between both given image pairs.
+     * \param source source image pair
+     * \param target target image pair
+     * \return 4x4 pose representing relative pose from
+     *         source to target
+     */
+    Eigen::Matrix4d estimate_pose(const rgbd_pair_t& source,
+      const rgbd_pair_t& target);
+
+  private:
+    cv::Mat m_camera_matrix;
+  }
+}
